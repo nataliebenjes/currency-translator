87520d6355da30f39564d0768bc8cacd18406648
Natalie Benjes <natalie.benjes@gmail.com>
2023-06-30 10:47:51 -0700

    add getTranslation to buisness logic. Add handleFormSubmission and error printing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import TranslateService from './translator.js';
 
+// Business Logic
+
+function getTranslation(phrase) {
+  let promise = TranslateService.getTranslation(phrase);
+  promise.then(function (translation) {
+    printElements(translation);
+  }, function (errorArray) {
+    printError(errorArray);
+  });
+}
+
+// UI Logic
+
+function printElements(translation) {
+  document.querySelector('#showResponse').innerText = `Your translated phrase is ${translation}`
+}
+
+function printError(error) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const phrase = document.querySelector('#entryad').value;
+  document.querySelector('#entry').value = null;
+  getTranslation(phrase);
+}
+
+window.addEventListener("load", function () {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
